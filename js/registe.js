5f00b94ce70eae34e8fb668e246ef13fe5e17880
sxt12138 <1310584836@qq.com>
2018-06-14 08:50:07 +0800

    更新4

---
+++ js/registe.js
@@ -0,0 +1,47 @@
+window.onload = function(){
+		go();
+		erw();
+		gie();
+	}
+	function go(){
+	let g = document.getElementById("huaguo1");
+	let h = document.getElementById("chuxian1");
+	g.onmouseover = function(){
+		h.style.display = "block";
+	}
+	g.onmouseout = function(){
+		h.style.display = "none";
+	}
+	h.onmouseover = function(){
+		h.style.display = "block";
+	}
+	h.onmouseout = function(){
+		h.style.display = "none";
+	}
+}
+function erw(){
+	let g = document.getElementById("hh");
+	let h = document.getElementById("ding");
+	g.onmouseover = function(){
+		h.style.display = "block";
+	}
+	g.onmouseout = function(){
+		h.style.display = "none";
+	}
+}
+function gie(){
+	let g = document.getElementById("huaguo2");
+	let h = document.getElementById("chuxian2");
+	g.onmouseover = function(){
+		h.style.display = "block";
+	}
+	g.onmouseout = function(){
+		h.style.display = "none";
+	}
+	h.onmouseover = function(){
+		h.style.display = "block";
+	}
+	h.onmouseout = function(){
+		h.style.display = "none";
+	}
+} 
